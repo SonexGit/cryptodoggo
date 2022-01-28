45043e730a93332f0de4ce59a3be65409c387064
Allan <shyris@outlook.fr>
2022-01-28 19:14:06 +0100

    Fix icones Fontawesome

--- cryptofront/src/app/app.module.ts
+++ cryptofront/src/app/app.module.ts
@@ -6,7 +6,14 @@ import { AppComponent } from './app.component';
 import { CryptolineComponent } from './components/cryptoline/cryptoline.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+// On importe toute la librarie d'icônes de Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, far, fab);
 
 @NgModule({
   declarations: [
@@ -23,4 +30,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+	constructor(library: FaIconLibrary) {
+		library.addIconPacks(fas, far, fab);
+	}
+}
