f7af2c93fd21edfb9e90e76148434e215e9f1a0b
Allan <shyris@outlook.fr>
2022-01-29 23:35:24 +0100

    continuation js et api + ajout background

--- cryptofront/src/assets/app.js
+++ cryptofront/src/assets/app.js
@@ -9,32 +9,55 @@
 // });
 
 // charge le fichier json de test
-var data = null;
-function chargerJSON() {
-	$.getJSON("assets/data.json", function(json) {
-		data = json;
-	}, successJSON(data));
-}
-function successJSON(data) {
-	return data;
-}
-data = chargerJSON();
+var data;
+$.getJSON("assets/data.json", function(json) {
+	data = json.data;
+	generateCryptoList(data);
+});
 
-var json = require("assets/data.json");
-
-function addCryptoline() {
+function addCryptoline(element) {
 
 	// crée une balise div avec la classe accordion-item
-	var newDiv = document.createElement("div")
-	newDiv.className = "accordion-item";
+	var div = document.createElement("div")
+	div.className = "accordion-item";
 
 	// ajoute un header a la div
-	var newHeader = document.createElement("h2")
-	newHeader.className = "accordion-header";
-	newHeader.id = "panelsStayOpen-heading" + data[0].rank;
-	console.log(newHeader.id);
-	newDiv.appendChild(newHeader);
+	var header = document.createElement("h2")
+	header.className = "accordion-header";
+	header.id = "panelsStayOpen-heading" + element.rank;
+	div.appendChild(header);
+
+	// ajoute un bouton au header
+	var button = document.createElement("button");
+	button.className = "accordion-button collapsed";
+	button.type = "button";
+	button.setAttribute("data-bs-toggle", "collapse");
+	button.setAttribute("data-bs-target", "#panelsStayOpen-collapse" + element.rank);
+	button.ariaExpanded = false;
+	button.setAttribute("aria-controls", "panelsStayOpen-collapse" + element.rank);
+	button.innerHTML = element.name;
+	header.appendChild(button);
+
+	// crée une balise div pour le body de l'accordion
+	var divHead = document.createElement("div");
+	divHead.id = "panelsStayOpen-collapse" + element.rank;
+	divHead.className = "accordion-collapse collapse";
+	divHead.setAttribute("aria-labelledby", header.id);
+
+	// ajoute une div body
+	var divBody = document.createElement("div");
+	divBody.className = "accordion-body";
+
+	// ajoute l'intérieur du body
+	divBody.innerHTML = "<strong>GG</strong>";
+
+	divHead.appendChild(divBody);
+	header.append(divHead);
+
+	document.getElementById("accordionPanelsStayOpenExample").appendChild(div);
 
 }
 
-addCryptoline.call();
+function generateCryptoList(data) {
+	data.forEach(element => addCryptoline(element))
+}
